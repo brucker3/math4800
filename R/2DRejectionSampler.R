ce5f0236723e3f380a90bbdc720ee1f29f0c22a8
Bayard Rucker <bayardrucker0@gmail.com>
2018-12-06 14:57:25 -0600

    working on 2D sampler

--- R/2DRejectionSampler.R
+++ R/2DRejectionSampler.R
@@ -1,7 +1,7 @@
 #' math4800 project
 #' this file implemnts a funciton to sample from a joint  random variable that is defifned on a square
 #'
-#' @description This function returns a vector with n elemtns from a continus random variable
+#' @description This function returns a data frome with n elemtns from a joint continus random variable
 #'
 #' @param n number of observations. n must be an integer >0
 #' @param pdf a function that is the join  pdf of the distribution.
@@ -11,3 +11,41 @@
 #'
 #' @return data.frame of random deviate pairs
 #' @export
+
+
+jointRejectionSampler <- function(n, jpdf, a, b, C) {
+  return(print('test'))
+  samples <- data.frame(x = numeric(0),  y = numeric(0))
+
+  reject <- TRUE
+  while(reject) {
+    proposed <- runif(n = 2, min = a, max = b)
+    proposed.density <- do.call(jpdf, list(x = proposed[1], y = proposed[2]))
+    sampler.checker <- runif(n = 1, min = 0, max = C)
+
+  }
+  samples <- c(samples,proposed[!is.na(sim)])
+
+}
+
+
+mypdf <- function(x){  # should accept any real number and chekc that its a valid pdf
+  2*x    # this is integrate to 1 and be nonnegative 2 criteria
+}
+mypdf
+v <- integrate(mypdf,0,1) # check for valid pdf
+v[1] ==1
+jointtester <- jointRejectionSampler(10, mypdf , 0, 1, 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
